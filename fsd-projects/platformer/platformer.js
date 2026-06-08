4f93fb6605cd137af10c2d0dccaae5aee46eb71f
mariahe0807 <mariahe0807@gmail.com>
2026-06-08 20:28:48 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,22 +27,50 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    //toggleGrid();
 
 
     // TODO 2 - Create Platforms
 
-
+createPlatform(634,700,125,20, "green")
+createPlatform(450,610,125,20, "green")
+createPlatform(150,600,125,20, "green")
+createPlatform(300,450,125,20, "green")
+createPlatform(1200,630,125,20, "green")
+createPlatform(732,525,125,20, "green")
+createPlatform(900,650,125,20, "green")
+createPlatform(125,358,125,20, "green")
+createPlatform(600,400,125,20, "green")
+createPlatform(850,275,125,20, "green")
+createPlatform(743,125,125,20, "green")
+createPlatform(977,493,125,20, "green")
+createPlatform(1200,332,125,20, "green")
+createPlatform(1039,180,125,20, "green")
+createPlatform(450,300,125,20, "green")
+createPlatform(250,200,125,20, "green")
+createPlatform(500,100,125,20, "green")
+createPlatform(1300,125,125,20, "green")
+createPlatform(0,280,125,20, "green")
+createPlatform(50,100,125,20, "green")
 
 
     // TODO 3 - Create Collectables
 
+createCollectable("database",1350,70,1,1);
+createCollectable("database",200,170,.5,.4);
+createCollectable("database",50,50,0.5,1);
+createCollectable("database",1035,450,1,1);
+createCollectable("database",800,70,1,.7);
+createCollectable("database",510,560,2,1);
 
 
     
     // TODO 4 - Create Cannons
 
-
+createCannon("top",400,2000);
+createCannon("right",300,5000);
+createCannon("left",500,3000);
+createCannon("top",1000,4000)
     
     
     //////////////////////////////////
